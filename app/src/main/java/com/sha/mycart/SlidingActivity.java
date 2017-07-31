4f07909211f1a503e44530874bd521cf5d699b09
Qasim Ali <qasimali0001@outlook.com>
2017-08-01 00:01:43 +0500

    blur image successfully added

---
+++ app/src/main/java/com/sha/mycart/SlidingActivity.java
@@ -0,0 +1,13 @@
+package com.sha.mycart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlidingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliding);
+    }
+}
--- app/src/main/java/com/sha/mycart/logoActivity.java
+++ app/src/main/java/com/sha/mycart/logoActivity.java
@@ -1,28 +1,86 @@
 package com.sha.mycart;
 
+import android.annotation.TargetApi;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsicBlur;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.sha.mycart.blur.blurbulider;
 
 public class logoActivity extends AppCompatActivity {
-
+    ImageView image;
+    private RelativeLayout discriptionn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo2);
-        ImageView imageView = (ImageView) findViewById(R.id.image);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.blur);
-        blurbulider blur=new blurbulider();
+        image = (ImageView) findViewById(R.id.picture);
+        image.setImageResource(R.drawable.blurimage);
+        discriptionn = (RelativeLayout) findViewById(R.id.layout);
+        applyBlur();
+    }
+    private void applyBlur() {
+        image.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                image.getViewTreeObserver().removeOnPreDrawListener(this);
+                image.buildDrawingCache();
 
-            blur.blur(getApplicationContext(),bitmap);
+                Bitmap bmp = image.getDrawingCache();
+                blur(bmp, discriptionn);
+                return true;
+            }
+        });
+    }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void blur(Bitmap bkg, View view) {
+        long startMs = System.currentTimeMillis();
 
-        imageView.setImageBitmap(bitmap);
+        float radius = 20;
 
+        Bitmap overlay = Bitmap.createBitmap((int) (view.getMeasuredWidth()),
+                (int) (view.getMeasuredHeight()), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(overlay);
+
+        canvas.translate(-view.getLeft(), -view.getTop());
+        canvas.drawBitmap(bkg, 0, 0, null);
+
+        RenderScript rs = RenderScript.create(this);
+
+        Allocation overlayAlloc = Allocation.createFromBitmap(
+                rs, overlay);
+
+        ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(
+                rs, overlayAlloc.getElement());
+
+        blur.setInput(overlayAlloc);
+
+        blur.setRadius(radius);
+
+        blur.forEach(overlayAlloc);
+
+        overlayAlloc.copyTo(overlay);
+
+        view.setBackground(new BitmapDrawable(
+                getResources(), overlay));
+
+        rs.destroy();
 
     }
+
+
+
 }
